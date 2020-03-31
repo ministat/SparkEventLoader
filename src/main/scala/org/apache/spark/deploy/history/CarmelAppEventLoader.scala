0829274a641909cd5cc140d29b076a36dbc1f7d8
Hongjiang Zhang <hongjizhang@ebay.com>
2020-03-31 23:59:18 +0800

    allow to run for hdfs

--- src/main/scala/org/apache/spark/deploy/history/CarmelAppEventLoader.scala
+++ src/main/scala/org/apache/spark/deploy/history/CarmelAppEventLoader.scala
@@ -1,14 +1,17 @@
 package org.apache.spark.deploy.history
 
 import java.io.{BufferedInputStream, Closeable, InputStream}
+import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.concurrent.{ExecutorService, Future}
 
 import com.google.common.util.concurrent.MoreExecutors
 import org.apache.hadoop.fs.{FileStatus, FileSystem, Path}
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
 import org.apache.spark.deploy.SparkHadoopUtil
 import org.apache.spark.deploy.history.FsHistoryProvider.{APPL_END_EVENT_PREFIX, APPL_START_EVENT_PREFIX, ENV_UPDATE_EVENT_PREFIX, LOG_START_EVENT_PREFIX}
-import org.apache.spark.deploy.history.HistoryServer.conf
+import org.apache.spark.deploy.history.HistoryServer.{conf, initSecurity}
 import org.apache.spark.deploy.history.config.EVENT_LOG_DIR
 import org.apache.spark.internal.Logging
 import org.apache.spark.io.CompressionCodec
@@ -24,8 +27,17 @@ class MyArgumentsParser{
   @ArgsOpt(name="-i", aliases=Array("--input-dir"), required=true, usage="Specify the history log directory")
   var inputDir: String = null
 
+  @ArgsOpt(name="-b", aliases=Array("--before-date"), required=false, usage="Specify the before date: 2020-03-25 17:44:02")
+  var beforeDate: String = null //new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis())
+
+  @ArgsOpt(name="-a", aliases=Array("--after-date"), required=false, usage="Specify the after date: 2020-03-25 17:24:02")
+  var afterDate: String = null //new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis() - 24 * 3600 * 1000)
+
   @ArgsOpt(name="-o", aliases=Array("--output-dir"), required=false, usage="Specify the output directory")
   var outputDir: String = null
+
+  @ArgsOpt(name="-l", aliases=Array("--local"), required=false, usage="Run under local filesystem instead of HDFS")
+  var localRun: Boolean = true
 
   def doParse(arguments: Array[String]): Unit = {
     val parser = new CmdLineParser(this)
@@ -45,14 +57,46 @@ class MyArgumentsParser{
 
 object CarmelAppEventLoader extends Logging {
 
+  private val conf = new SparkConf
+
+  def initSecurity() {
+    // If we are accessing HDFS and it has security enabled (Kerberos), we have to login
+    // from a keytab file so that we can access HDFS beyond the kerberos ticket expiration.
+    // As long as it is using Hadoop rpc (hdfs://), a relogin will automatically
+    // occur from the keytab.
+    if (conf.getBoolean("spark.history.kerberos.enabled", false)) {
+      // if you have enabled kerberos the following 2 params must be set
+      val principalName = conf.get("spark.history.kerberos.principal")
+      val keytabFilename = conf.get("spark.history.kerberos.keytab")
+      SparkHadoopUtil.get.loginUserFromKeytab(principalName, keytabFilename)
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val argumentsParser = new MyArgumentsParser()
     argumentsParser.doParse(args)
-    val logDir= argumentsParser.inputDir
-    val conf = new SparkConf
-    conf.set(EVENT_LOG_DIR, argumentsParser.inputDir)
+    if (argumentsParser.localRun) {
+      val localConf = new SparkConf
+      localConf.set(EVENT_LOG_DIR, argumentsParser.inputDir)
 
-    val carmelFsHistoryProvider = new CarmelFsHistoryProvider(conf)
-    carmelFsHistoryProvider.loadAllLogs()
+      val carmelFsHistoryProvider = new CarmelFsHistoryProvider(localConf)
+      val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+      val beforeDate = if (argumentsParser.beforeDate != null) simpleDateFormat.parse(argumentsParser.beforeDate) else null
+      val afterDate = if (argumentsParser.afterDate != null) simpleDateFormat.parse(argumentsParser.afterDate) else null
+      carmelFsHistoryProvider.loadAllLogs(afterDate, beforeDate)
+      carmelFsHistoryProvider.getListing().foreach(println(_))
+      println(carmelFsHistoryProvider.getApplicationCount())
+    } else {
+      initSecurity()
+      val providerName = conf.getOption("spark.history.provider")
+        .getOrElse(classOf[FsHistoryProvider].getName())
+      val provider = Utils.classForName(providerName)
+        .getConstructor(classOf[SparkConf])
+        .newInstance(conf)
+        .asInstanceOf[ApplicationHistoryProvider]
+      println(provider.getListing().length)
+      provider.getListing().foreach(println(_))
+    }
+
   }
 }
--- src/main/scala/org/apache/spark/deploy/history/CarmelFsHistoryProvider.scala
+++ src/main/scala/org/apache/spark/deploy/history/CarmelFsHistoryProvider.scala
@@ -6,22 +6,27 @@ import java.util.concurrent.{ExecutorService, Future}
 
 import com.google.common.util.concurrent.MoreExecutors
 import org.apache.hadoop.fs.{FileStatus, FileSystem, Path}
+import org.apache.hadoop.hdfs.DistributedFileSystem
 import org.apache.spark.SparkConf
 import org.apache.spark.deploy.SparkHadoopUtil
+import org.apache.spark.deploy.history.FsHistoryProvider.CURRENT_LISTING_VERSION
 import org.apache.spark.deploy.history.config.LOCAL_STORE_DIR
 import org.apache.spark.scheduler.EventLoggingListener
 import org.apache.spark.scheduler.ReplayListenerBus.SELECT_ALL_FILTER
-import org.apache.spark.util.SystemClock
+import org.apache.spark.util.{Clock, SystemClock, Utils}
 import org.apache.spark.deploy.history.config.EVENT_LOG_DIR
 import org.apache.spark.internal.Logging
 import org.apache.spark.io.CompressionCodec
 import org.apache.spark.scheduler.ReplayListenerBus.ReplayEventsFilter
 import org.apache.spark.scheduler.{ReplayListenerBus, SparkListener, SparkListenerApplicationEnd, SparkListenerApplicationStart, SparkListenerEnvironmentUpdate, SparkListenerEvent, SparkListenerLogStart}
+import org.apache.spark.status.AppStatusStore
+import org.apache.spark.status.KVUtils.{MetadataMismatchException, open}
 import org.apache.spark.status.api.v1.{ApplicationAttemptInfo, ApplicationInfo}
-import org.apache.spark.util.Clock
+import org.apache.spark.util.kvstore.{InMemoryStore, KVStore, UnsupportedStoreVersionException}
+import org.fusesource.leveldbjni.internal.NativeDB
 
 import scala.collection.mutable
-
+import scala.collection.JavaConverters._
 
 private[history] class CarmelAppListingListener(log: FileStatus, clock: Clock) extends SparkListener {
 
@@ -42,7 +47,7 @@ private[history] class CarmelAppListingListener(log: FileStatus, clock: Clock) e
     attempt.endTime = new Date(event.time)
     attempt.lastUpdated = new Date(log.getModificationTime())
     attempt.duration = event.time - attempt.startTime.getTime()
-    println(s"${app.name} with ${attempt.sparkUser} runs ${attempt.duration} from ${attempt.startTime.toString} to ${attempt.endTime.toString} in queue ${attempt.sparkQueue.toString}")
+    //println(s"${app.name} with ${attempt.sparkUser} runs ${attempt.duration} from ${attempt.startTime.toString} to ${attempt.endTime.toString} in queue ${attempt.sparkQueue.toString}")
     attempt.completed = true
   }
 
@@ -132,6 +137,76 @@ private[history] class CarmelFsHistoryProvider(conf: SparkConf, clock: Clock) ex
   private val hadoopConf = SparkHadoopUtil.get.newConfiguration(conf)
   private val codecMap = new mutable.HashMap[String, CompressionCodec]
   private val storePath = conf.get(LOCAL_STORE_DIR).map(new File(_))
+  private val logDir = conf.get(EVENT_LOG_DIR)
+
+  def getListing(): Iterator[ApplicationInfo] = {
+    // Return the listing in end time descending order.
+    listing.view(classOf[ApplicationInfoWrapper])
+      .index("endTime")
+      .reverse()
+      .iterator()
+      .asScala
+      .map(_.toApplicationInfo())
+  }
+
+  def getApplicationCount(): Long = {
+    return listing.count(classOf[ApplicationInfoWrapper])
+  }
+
+  private[history] val listing: KVStore = storePath.map { path =>
+    require(path.isDirectory(), s"Configured store directory ($path) does not exist.")
+    val dbPath = new File(path, "listing.ldb")
+    val metadata = new FsHistoryProviderMetadata(CURRENT_LISTING_VERSION,
+      AppStatusStore.CURRENT_VERSION, logDir.toString())
+
+    try {
+      open(dbPath, metadata)
+    } catch {
+      // If there's an error, remove the listing database and any existing UI database
+      // from the store directory, since it's extremely likely that they'll all contain
+      // incompatible information.
+      case _: UnsupportedStoreVersionException | _: MetadataMismatchException =>
+        logInfo("Detected incompatible DB versions, deleting...")
+        path.listFiles().foreach(Utils.deleteRecursively)
+        open(dbPath, metadata)
+      case dbExc: NativeDB.DBException =>
+        // Get rid of the corrupted listing.ldb and re-create it.
+        logWarning(s"Failed to load disk store $dbPath :", dbExc)
+        Utils.deleteRecursively(dbPath)
+        open(dbPath, metadata)
+    }
+  }.getOrElse(new InMemoryStore())
+
+  /**
+   * Write the app's information to the given store. Serialized to avoid the (notedly rare) case
+   * where two threads are processing separate attempts of the same application.
+   */
+  private def addListing(app: ApplicationInfoWrapper): Unit = listing.synchronized {
+    val attempt = app.attempts.head
+
+    val oldApp = try {
+      load(app.id)
+    } catch {
+      case _: NoSuchElementException =>
+        app
+    }
+
+    def compareAttemptInfo(a1: AttemptInfoWrapper, a2: AttemptInfoWrapper): Boolean = {
+      a1.info.startTime.getTime() > a2.info.startTime.getTime()
+    }
+
+    val attempts = oldApp.attempts.filter(_.info.attemptId != attempt.info.attemptId) ++
+      List(attempt)
+
+    val newAppInfo = new ApplicationInfoWrapper(
+      app.info,
+      attempts.sortWith(compareAttemptInfo))
+    listing.write(newAppInfo)
+  }
+
+  private def load(appId: String): ApplicationInfoWrapper = {
+    listing.read(classOf[ApplicationInfoWrapper], appId)
+  }
 
   private val replayExecutor: ExecutorService = {
     MoreExecutors.sameThreadExecutor()
@@ -195,10 +270,29 @@ private[history] class CarmelFsHistoryProvider(conf: SparkConf, clock: Clock) ex
     val listener = new CarmelAppListingListener(fileStatus, clock)
     bus.addListener(listener)
     replay(fileStatus, bus, fs, eventsFilter)
+
+    val (appId, attemptId) = listener.applicationInfo match {
+      case Some(app) =>
+        // Invalidate the existing UI for the reloaded app attempt, if any. See LoadedAppUI for a
+        // discussion on the UI lifecycle.
+
+
+        addListing(app)
+        (Some(app.info.id), app.attempts.head.info.attemptId)
+
+      case _ =>
+        // If the app hasn't written down its app ID to the logs, still record the entry in the
+        // listing db, with an empty ID. This will make the log eligible for deletion if the app
+        // does not make progress after the configured max log age.
+        (None, None)
+    }
   }
 
-  def loadAllLogs(): Unit = {
+  def loadAllLogs(after: Date, before: Date): Unit = {
     val logDir = conf.get(EVENT_LOG_DIR)
+    hadoopConf.set("fs.hdfs.impl",
+      classOf[DistributedFileSystem].getName)
+    println(hadoopConf.get("fs.hdfs.impl"))
     val fs = new Path(logDir).getFileSystem(hadoopConf)
 
     val updated = Option(fs.listStatus(new Path(logDir))).map(_.toSeq).getOrElse(Nil)
@@ -208,6 +302,21 @@ private[history] class CarmelFsHistoryProvider(conf: SparkConf, clock: Clock) ex
           // reading a garbage file is safe, but we would log an error which can be scary to
           // the end-user.
           !entry.getPath().getName().startsWith(".")
+      }
+      .filter { entry =>
+        val modTime = new Date(entry.getModificationTime)
+        if (after != null && before != null) {
+          modTime.after(after) && modTime.before(before)
+        } else if (after != null) {
+          modTime.after(after)
+        } else if (before != null ) {
+          modTime.before(before)
+        } else {
+          true
+        }
+      }
+      .filter { entry =>
+        entry.getPath().getName().endsWith(".lz4")
       }
       .sortWith { case (entry1, entry2) =>
         entry1.getModificationTime() > entry2.getModificationTime()
